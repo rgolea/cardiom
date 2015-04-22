32e99980b11d77bee5db77dbf101bb9c450cac71
Rares G <rgolea@gmail.com>
2015-04-22 14:32:16 +0200

    ACLS boot script completed

--- server/boot/overwriteACLS.js
+++ server/boot/overwriteACLS.js
@@ -1,16 +1,34 @@
+var fs = require('fs');
+var path = require('path');
 module.exports = function (app) {
     var modelNames = Object.keys(app.models);
-    modelNames.forEach(function (m) {
-        var model = app.models[m];
-        var config = require('../../common/models/' + model.modelName.toLowerCase() + '.json');
-        
-        if (model.settings.overwriteACLS) {
-                model.settings.acls = [];
-                config.acls.forEach(function (r) {
-                    model.settings.acls.push(r);
-                });
-            console.log('done');
+
+    var modelFolder = './common/models/';
+    fs.readdir(modelFolder, function (err, list) {
+        if (err) {
+            console.log(err);
         };
+
+        var models = [];
+        
+        list.forEach(function (file) {
+            if (file.indexOf('.json')!==-1) {
+                var jsonFile = require('../../common/models/'+file);
+
+                modelNames.forEach(function (m) {
+                    var model = app.models[m];
+                    if(jsonFile.name == model.modelName && model.settings.overwriteACLS){
+                        model.settings.acls = [];
+                        jsonFile.acls.forEach(function (r) {
+                            model.settings.acls.push(r);
+                        });
+                    };
+                });
+
+
+            }
+
+        });
     });
 
 };
